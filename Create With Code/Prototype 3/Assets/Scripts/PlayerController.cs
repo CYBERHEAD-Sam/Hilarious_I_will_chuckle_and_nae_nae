572c65fe641b7141d054a729ae38636ae1815a83
SamIsLost <sara.r433@stu.nebo.edu>
2019-11-18 10:05:31 -0700

    wah

--- Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -21,16 +23,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) { 
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) { 
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
         
     }
 //you can call it collision or anything you want- it doesnt matter.
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
 
         if(collision.gameObject.CompareTag("Ground"))
         {
@@ -38,6 +40,8 @@ public class PlayerController : MonoBehaviour
         } else if(collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over, foo.");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
             gameOver = true;
         }
 
